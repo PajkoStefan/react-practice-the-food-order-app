d33da182591d15aa807aafb2502930f676e6958f
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2022-07-11 22:06:11 +0200

    Animation on Cart btn added

--- src/components/Layout/Header/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../../../../assets/icons/CartIcon/CartIcon";
 import CartContext from "../../../../store/cart-context.js/cart-context";
 import Button from "../../../UI/Button/Button";
@@ -6,17 +6,33 @@ import Button from "../../../UI/Button/Button";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = ({ onClick }) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const { items } = useContext(CartContext);
 
   const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted && classes.bump}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timeoutId = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => clearTimeout(timeoutId);
+  }, [items]);
+
   return (
     <Button
       button={{
         id: "btn-show-cart",
-        className: classes.button,
+        className: btnClasses,
         onClick: onClick,
       }}
     >
